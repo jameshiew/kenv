f2d03c915c09e468367c7e59cffb608c31d46f1f
James Hiew <james@hiew.net>
2020-01-07 02:05:11 +0000

    cmd: print a better error message when the kenv func might not be set

--- cmd/shell.go
+++ cmd/shell.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/jameshiew/kenv/cmd/internal/utils"
 	"github.com/jameshiew/kenv/internal"
 
@@ -11,8 +13,12 @@ var shellCmd = &cobra.Command{
 	Use:   "shell",
 	Short: "See or set the shell kubectl version",
 	Long:  "See or set the shell kubectl version",
-	Args:  cobra.NoArgs, // setting the shell version must be done externally
+	Args:  cobra.MaximumNArgs(1), // setting the shell version must be done externally
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 1 {
+			cmd.PrintErrln("Cannot set shell version - has `eval $(kenv init -)` been run for this shell session?")
+			os.Exit(1)
+		}
 		utils.PrintAndExit(cmd, internal.ShellVersion)
 	},
 }
